1d9cf2bf04e7c398747a0d843d6a1d1b988feeed
Mian Jazib Ali <m.mianjazibali@gmail.com>
2022-11-17 20:45:02 +0100

    Style Event List and Item

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import classes from './event-item.module.css';
 
 const EventItem = (props) => {
   const { id, title, location, date, image } = props;
@@ -8,19 +9,19 @@ const EventItem = (props) => {
   const formattedLocation = location.replace(', ', '\n');
 
   return (
-    <li>
+    <li className={classes.item}>
       <Image src={`/${image}`} alt={title} width="200" height="200" />
       <div>
-        <div>
+        <div className={classes.content}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{formattedDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedLocation}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={`/events/${id}`}>Explore Event</Link>
         </div>
       </div>
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,8 +1,9 @@
 import EventItem from './event-item';
+import classes from './event-list.module.css';
 
 const EventList = ({ events }) => {
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map((event) => (
         <EventItem key={event.id} {...event} />
       ))}
