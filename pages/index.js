fcd8bf2d8305e43ba69f4e8294083b9f24cab3a3
Mian Jazib Ali <m.mianjazibali@gmail.com>
2022-12-11 12:52:04 +0100

    Use Static Page Generation For Featured Events

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-utils';
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
-  return <EventList events={featuredEvents} />;
+const HomePage = ({ events }) => {
+  return <EventList events={events} />;
 };
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return { props: { events: featuredEvents }, revalidate: 600 };
+}
 
 export default HomePage;
